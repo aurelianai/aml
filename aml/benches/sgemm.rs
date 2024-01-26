5a309964bf0cd2368fb0dfc479ccfa8a947ebb21
Ethan Steere <62357808+e253@users.noreply.github.com>
2024-01-26 15:41:59 -0500

    fixes before attempting to do blocking / parallelism optims.

--- aml/benches/sgemm.rs
+++ aml/benches/sgemm.rs
@@ -5,17 +5,17 @@ use rand::{thread_rng, Rng};
 pub fn sgemm_benches(cri: &mut Criterion) {
     let mut rng = thread_rng();
 
-    let mut a_data: Vec<f32> = vec![0.0; 4096 * 1024];
+    let mut a_data: Vec<f32> = vec![0.0; 1024 * 256];
     a_data.iter_mut().for_each(|v| *v = rng.gen());
-    let a = F32Tensor::new(&a_data, vec![1024, 4096]);
+    let a = F32Tensor::new(&a_data, vec![1024, 256]);
 
-    let mut b_data: Vec<f32> = vec![0.0; 4096 * 512];
+    let mut b_data: Vec<f32> = vec![0.0; 256 * 128];
     b_data.iter_mut().for_each(|v| *v = rng.gen());
-    let b = F32Tensor::new(&b_data, vec![4096, 512]);
+    let b = F32Tensor::new(&b_data, vec![256, 128]);
 
-    let mut c: Vec<f32> = vec![0.0; 512 * 1024];
+    let mut c: Vec<f32> = vec![0.0; 1024 * 128];
 
-    cri.bench_function("Large Rectangular Matrices", |bencher| {
+    cri.bench_function("Medium Rectangular Matrices", |bencher| {
         bencher.iter(|| {
             sgemm(
                 black_box(&a),
--- aml/src/lib.rs
+++ aml/src/lib.rs
@@ -10,6 +10,8 @@ impl<'a> F32Tensor<'a> {
     /// Utility Method eliminating footguns assoc. with creating tensors by hand
     pub fn new(data: &'a [f32], shape: Vec<usize>) -> F32Tensor<'a> {
         assert!(shape.len() == 2, "Only Shapes of length 2 are supported");
+        assert!(shape[0] % 16 == 0, "Dim 0 {} must be divisible by 16", shape[0]);
+        assert!(shape[1] % 16 == 0, "Dim 1 {} must be divisible by 16", shape[1]);
         assert!(
             data.len() == shape.iter().fold(1, |acc, next| acc * next),
             "Data of Length {} doesn't work for shape {:#?}",
@@ -22,22 +24,29 @@ impl<'a> F32Tensor<'a> {
 }
 
 pub fn sgemm(a: &F32Tensor, a_t: bool, b: &F32Tensor, b_t: bool, c: &mut Vec<f32>) {
+    assert!(!a_t && !b_t, "Transposes are not supported yet");
     assert!(
         a.shape[1] == b.shape[0],
         "Tensor A Shape {:#?} is not compatible with Tensor B Shape {:#?}",
         a.shape,
         b.shape
     );
-    assert!(!a_t && !b_t, "Transposes are not supported yet");
+    assert!(
+        a.shape[0] * b.shape[1] == c.len(),
+        "Output buffer `c` has size {}, but should have {} * {}",
+        c.len(),
+        a.shape[0],
+        b.shape[1]
+    );
 
     let m = a.shape[0];
     let n = a.shape[1];
     let p = b.shape[1];
 
     for i in 0..m {
-        for j in 0..n {
-            for k in 0..p {
-                c[i * p + k] += a.data[i * n + j] * b.data[j * p + k];
+        for j in 0..p {
+            for k in 0..n {
+                c[i * p + j] += a.data[i * n + k] * b.data[k * p + j];
             }
         }
     }

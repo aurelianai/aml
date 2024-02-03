c53f8758a2a903f2bc337e455805b68e07bcaff9
Ethan Steere <62357808+e253@users.noreply.github.com>
2024-02-02 20:04:28 -0500

    Attempted to make vectorization better. Added a stub for a kernel that works on column major `B`.

--- aml/src/lib.rs
+++ aml/src/lib.rs
@@ -36,6 +36,22 @@ impl<'a> F32Tensor<'a> {
 
         Self { shape, data }
     }
+    /* 
+    pub fn to_col_major(self: &Self) -> F32Tensor {
+        let mut new_data: Vec<f32> = Vec::new();
+        
+        let p = self.shape[0];
+        let n = self.shape[1];
+        
+        for row_offset in 0..p  {
+            for row in (0..n * p).step_by(p) {
+                new_data.push(self.data[row + row_offset]);
+            }
+        }
+        
+        F32Tensor::new(&Vec::from(new_data), self.shape)
+    }
+    */
 }
 
 #[derive(Copy)]
@@ -172,7 +188,7 @@ pub fn sgemm_tiled_par(a: &F32Tensor, a_t: bool, b: &F32Tensor, b_t: bool, c: &m
                                                 *a.data.get_unchecked(row * n + tile + tile_row)
                                                     * *b.data.get_unchecked(
                                                         tile * p + tile_row * p + col_block + el,
-                                                    )
+                                                    ),
                                             );
                                         }
                                     }
@@ -186,6 +202,8 @@ pub fn sgemm_tiled_par(a: &F32Tensor, a_t: bool, b: &F32Tensor, b_t: bool, c: &m
     }
 }
 
+/// The other functions are counterintuitively far more vectorized that this one
+/// The loops are agressively unrolled in a way that would be hard for me to match.
 pub fn sgemm_tiled_simd(a: &F32Tensor, a_t: bool, b: &F32Tensor, b_t: bool, c: &mut Vec<f32>) {
     assert!(!a_t && !b_t, "Transposes are not supported yet");
     assert!(
@@ -207,34 +225,40 @@ pub fn sgemm_tiled_simd(a: &F32Tensor, a_t: bool, b: &F32Tensor, b_t: bool, c: &
     let p = b.shape[1];
 
     let block_size = 16;
+    
+    let a_ptr = a.data.as_ptr();
+    let b_ptr = b.data.as_ptr();
+    let c_ptr = c.as_mut_ptr();
 
     if is_x86_feature_detected!("avx") {
         for col_block in (0..p).step_by(block_size) {
             for row in 0..m {
                 for tile in (0..n).step_by(block_size) {
-                    for tile_col in 0..block_size {
-                        unsafe {
-                            let b_vector_1 = _mm256_loadu_ps(
-                                b.data.as_ptr().add(tile * p + tile_col * p + col_block),
+                    unsafe {
+                        let mut c_1 = _mm256_loadu_ps(c_ptr.add(row * p + col_block));
+                        let mut c_2 = _mm256_loadu_ps(c_ptr.add(row * p + col_block + 8));
+                        for tile_col in 0..block_size {
+                            let b_1 = _mm256_loadu_ps(
+                                b_ptr.add(tile * p + tile_col * p + col_block),
                             );
-                            let b_vector_2 = _mm256_loadu_ps(
-                                b.data.as_ptr().add(tile * p + tile_col * p + col_block + 8),
-                            );
-
-                            let a_values = _mm256_broadcast_ss(&a.data[row * n + tile + tile_col]);
-
-                            let res_1 = _mm256_add_ps(
-                                _mm256_mul_ps(a_values, b_vector_1),
-                                _mm256_loadu_ps(c.as_ptr().add(row * p + col_block)),
-                            );
-                            let res_2 = _mm256_add_ps(
-                                _mm256_mul_ps(a_values, b_vector_2),
-                                _mm256_loadu_ps(c.as_ptr().add(row * p + col_block + 8)),
+                            let b_2 = _mm256_loadu_ps(
+                                b_ptr.add(tile * p + tile_col * p + col_block + 8),
                             );
 
-                            _mm256_storeu_ps(c.as_mut_ptr().add(row * p + col_block), res_1);
-                            _mm256_storeu_ps(c.as_mut_ptr().add(row * p + col_block + 8), res_2);
+                            let a_val = _mm256_broadcast_ss(&*a_ptr.add(row * n + tile + tile_col));
+
+                            c_1 = _mm256_add_ps(
+                                c_1,
+                                _mm256_mul_ps(a_val, b_1),
+                            );
+                            c_2 = _mm256_add_ps(
+                                c_2,
+                                _mm256_mul_ps(a_val, b_2),
+                            );
+
                         }
+                        _mm256_storeu_ps(c.as_mut_ptr().add(row * p + col_block), c_1);
+                        _mm256_storeu_ps(c.as_mut_ptr().add(row * p + col_block + 8), c_2);
                     }
                 }
             }
@@ -250,6 +274,37 @@ pub fn sgemm_tiled_simd(a: &F32Tensor, a_t: bool, b: &F32Tensor, b_t: bool, c: &
                         }
                     }
                 }
+            }
+        }
+    }
+}
+
+// Column Major Functions
+
+/// WIP, just a copy of regular function
+pub fn sgemm_cm(a: &F32Tensor, b: &F32Tensor, c: &mut Vec<f32>) {
+    assert!(
+        a.shape[1] == b.shape[0],
+        "Tensor A Shape {:#?} is not compatible with Tensor B Shape {:#?}",
+        a.shape,
+        b.shape
+    );
+    assert!(
+        a.shape[0] * b.shape[1] == c.len(),
+        "Output buffer `c` has size {}, but should have {} * {}",
+        c.len(),
+        a.shape[0],
+        b.shape[1]
+    );
+
+    let m = a.shape[0];
+    let n = a.shape[1];
+    let p = b.shape[1];
+    
+    for i in 0..m {
+        for j in 0..p {
+            for k in 0..n {
+                c[i * p + j] += a.data[i * n + k] * b.data[k * p + j];
             }
         }
     }

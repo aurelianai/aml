8be08cd84adc43d219e1f6fedb040d830038d6ad
Ethan Steere <62357808+e253@users.noreply.github.com>
2024-02-07 23:20:08 -0500

    WIP pure asm write of hot loop

--- aml/src/lib.rs
+++ aml/src/lib.rs
@@ -1,11 +1,7 @@
 #[cfg(test)]
 mod tests;
 
-#[cfg(target_arch = "x86")]
-use core::arch::x86;
-#[cfg(target_arch = "x86_64")]
-use core::arch::x86_64::*;
-
+use std::arch::asm;
 use std::thread;
 
 pub struct F32Tensor<'a> {
@@ -217,7 +213,7 @@ pub fn sgemm_tiled_simd(a: &F32Tensor, a_t: bool, b: &F32Tensor, b_t: bool, c: &
 
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
     {
-        if std::is_x86_feature_detected!("avx") {
+        if std::is_x86_feature_detected!("avx") && std::is_x86_feature_detected!("fma") {
             unsafe {
                 sgemm_avx(a_ptr, b_ptr, c_ptr, m, n, p, block_size);
             }
@@ -241,7 +237,7 @@ unsafe fn sgemm_avx512(
     for col_block in (0..p).step_by(block_size) {
         for row in 0..m {
             for tile in (0..n).step_by(block_size) {
-                core::arch::asm!(
+                asm!(
 
                 )
             }
@@ -264,64 +260,60 @@ unsafe fn sgemm_avx(
     for col_block in (0..p).step_by(block_size) {
         for row in 0..m {
             for tile in (0..n).step_by(block_size) {
-                let mut c_1 = _mm256_loadu_ps(c_ptr.add(row * p + col_block));
-                let mut c_2 = _mm256_loadu_ps(c_ptr.add(row * p + col_block + 8));
 
-                let mut a_1 = _mm_loadu_ps(a_ptr.add(row * n + tile));
-                let mut a_2 = _mm_loadu_ps(a_ptr.add(row * n + tile + 4));
-                let mut a_3 = _mm_loadu_ps(a_ptr.add(row * n + tile + 8));
-                let mut a_4 = _mm_loadu_ps(a_ptr.add(row * n + tile + 12));
+                // ymm0 + ymm1 = c
+                asm!("vmovups ymm0, [{}]", in(reg) c_ptr.add(row * p + col_block));
+                asm!("vmovups ymm1, [{}]", in(reg) c_ptr.add(row * p + col_block + 8));
 
-                let mut a_val = _mm256_broadcastss_ps(a_1);
+                // ymm4 = a_val
+                asm!("movups xmm5, [{}]", in(reg) a_ptr.add(row * n + tile));
+                asm!("movups xmm6, [{}]", in(reg) a_ptr.add(row * n + tile + 4));
+                asm!("movups xmm7, [{}]", in(reg) a_ptr.add(row * n + tile + 8));
+                asm!("movups xmm8, [{}]", in(reg) a_ptr.add(row * n + tile + 12));
+
+                // ymm2 + ymm3 = b
                 for tile_col in 0..4 {
-                    let b_1 = _mm256_loadu_ps(b_ptr.add(tile * p + tile_col * p + col_block));
-                    let b_2 = _mm256_loadu_ps(b_ptr.add(tile * p + tile_col * p + col_block + 8));
-
-                    c_1 = _mm256_fmadd_ps(a_val, b_1, c_1);
-                    c_2 = _mm256_fmadd_ps(a_val, b_2, c_2);
-
-                    a_1 = _mm_shuffle_ps(a_1, a_1, 0x39);
-                    a_val = _mm256_broadcastss_ps(a_1);
+                    asm!("vbroadcastss ymm4, xmm5");
+                    asm!("vmovups ymm2, [{}]", in(reg) b_ptr.add((tile + tile_col) * p + col_block));
+                    asm!("vmovups ymm3, [{}]", in(reg) b_ptr.add((tile + tile_col) * p + col_block + 8));
+                    asm!("vfmadd231ps ymm0, ymm2, ymm4");
+                    asm!("vfmadd231ps ymm1, ymm3, ymm4");
+                    asm!("shufps xmm5, xmm5, 57");
                 }
 
-                a_val = _mm256_broadcastss_ps(a_2);
                 for tile_col in 4..8 {
-                    let b_1 = _mm256_loadu_ps(b_ptr.add(tile * p + tile_col * p + col_block));
-                    let b_2 = _mm256_loadu_ps(b_ptr.add(tile * p + tile_col * p + col_block + 8));
-
-                    c_1 = _mm256_fmadd_ps(a_val, b_1, c_1);
-                    c_2 = _mm256_fmadd_ps(a_val, b_2, c_2);
-
-                    a_2 = _mm_shuffle_ps(a_2, a_2, 0x39);
-                    a_val = _mm256_broadcastss_ps(a_2);
+                    asm!("vbroadcastss ymm4, xmm6");
+                    asm!("vmovups ymm2, [{}]", in(reg) b_ptr.add((tile + tile_col) * p + col_block));
+                    asm!("vmovups ymm3, [{}]", in(reg) b_ptr.add((tile + tile_col) * p + col_block + 8));
+                    asm!("vfmadd231ps ymm0, ymm2, ymm4");
+                    asm!("vfmadd231ps ymm1, ymm3, ymm4");
+                    asm!("shufps xmm6, xmm6, 57");
                 }
 
-                a_val = _mm256_broadcastss_ps(a_3);
                 for tile_col in 8..12 {
-                    let b_1 = _mm256_loadu_ps(b_ptr.add(tile * p + tile_col * p + col_block));
-                    let b_2 = _mm256_loadu_ps(b_ptr.add(tile * p + tile_col * p + col_block + 8));
-
-                    c_1 = _mm256_fmadd_ps(a_val, b_1, c_1);
-                    c_2 = _mm256_fmadd_ps(a_val, b_2, c_2);
-
-                    a_3 = _mm_shuffle_ps(a_3, a_3, 0x39);
-                    a_val = _mm256_broadcastss_ps(a_3);
+                    asm!("vbroadcastss ymm4, xmm7");
+                    asm!("vmovups ymm2, [{}]", in(reg) b_ptr.add((tile + tile_col) * p + col_block));
+                    asm!("vmovups ymm3, [{}]", in(reg) b_ptr.add((tile + tile_col) * p + col_block + 8));
+                    asm!("vfmadd231ps ymm0, ymm2, ymm4");
+                    asm!("vfmadd231ps ymm1, ymm3, ymm4");
+                    asm!("shufps xmm7, xmm7, 57");
                 }
 
-                a_val = _mm256_broadcastss_ps(a_4);
                 for tile_col in 12..16 {
-                    let b_1 = _mm256_loadu_ps(b_ptr.add(tile * p + tile_col * p + col_block));
-                    let b_2 = _mm256_loadu_ps(b_ptr.add(tile * p + tile_col * p + col_block + 8));
-
-                    c_1 = _mm256_fmadd_ps(a_val, b_1, c_1);
-                    c_2 = _mm256_fmadd_ps(a_val, b_2, c_2);
-
-                    a_4 = _mm_shuffle_ps(a_4, a_4, 0x39);
-                    a_val = _mm256_broadcastss_ps(a_4);
+                    asm!("vbroadcastss ymm8, xmm3");
+                    asm!("vmovups ymm2, [{}]", in(reg) b_ptr.add((tile + tile_col) * p + col_block));
+                    asm!("vmovups ymm3, [{}]", in(reg) b_ptr.add((tile + tile_col) * p + col_block + 8));
+                    asm!("vfmadd231ps ymm0, ymm2, ymm4");
+                    asm!("vfmadd231ps ymm1, ymm3, ymm4");
+                    asm!("shufps xmm8, xmm8, 57");
                 }
 
-                _mm256_storeu_ps(c_ptr.add(row * p + col_block), c_1);
-                _mm256_storeu_ps(c_ptr.add(row * p + col_block + 8), c_2);
+                asm!(
+                    "vmovups [{0}], ymm0",
+                    "vmovups [{1}], ymm1",
+                    in(reg) c_ptr.add(row * p + col_block),
+                    in(reg) c_ptr.add(row * p + col_block + 8),
+                );
             }
         }
     }
